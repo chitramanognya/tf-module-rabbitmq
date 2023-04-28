a4ea9498f1ec576e17d60062525ac0c4d9ac3593
chitra <chmanu.chinu@gmail.com>
2023-04-28 14:15:04 +0000

    update rabbitmq

--- data.tf
+++ data.tf
@@ -3,3 +3,7 @@ data "aws_ami" "ami" {
     name_regex = "Centos-8-DevOps-Practice"
     owners     = ["self"]
 }
+
+data "aws_route53_zone" "domain" {
+    name = var.dns_domain
+}
--- vars.tf
+++ vars.tf
@@ -1,5 +1,9 @@
 variable "env" {}
- variable "subnet_ids" {}
-  variable "tags" {}
-  variable "instance_type" {}
+variable "subnet_ids" {}
+variable "tags" {}
+variable "instance_type" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
+variable "dns_domain" {}
   
